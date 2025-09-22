42b41ac4be5c6a846821b720c56967dfe42a0240
GitHub Action <action@github.com>
2025-09-22 03:32:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/pinkfloyda/intellirepl/MyBundle.kt
+++ src/main/kotlin/com/github/pinkfloyda/intellirepl/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pinkfloyda.intellirepl
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/pinkfloyda/intellirepl/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/pinkfloyda/intellirepl/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.pinkfloyda.intellirepl.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
